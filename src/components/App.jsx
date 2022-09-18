61574f493206d7e3097703d2ac07b874c34fbd6e
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-09-18 19:38:26 +0300

    Added main app logic and markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,66 @@
-export const App = () => {
-  return <></>;
-};
+import { Component } from 'react';
+import { FeedbackOptions, Section, Statistics } from './AllComponents';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  total = 0;
+  positivePercentage = null;
+
+  calcPositivePercentage() {
+    const { good } = this.state;
+    const { total } = this;
+
+    if (!total) return;
+
+    this.positivePercentage = parseInt((good / total) * 100);
+  }
+
+  calcTotalNumberFeedbacks() {
+    const { good, neutral, bad } = this.state;
+
+    this.total = good + neutral + bad;
+  }
+
+  onFeedbackOptClicked = type => {
+    this.setState(prevState => {
+      return { [type]: prevState[type] + 1 };
+    });
+  };
+
+  onRender() {
+    this.calcTotalNumberFeedbacks();
+    this.calcPositivePercentage();
+  }
+
+  render() {
+    this.onRender();
+    const { good, neutral, bad } = this.state;
+    const { total, positivePercentage } = this;
+
+    return (
+      <>
+        <Section title="Please leave a feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onFeedbackOptClicked}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      </>
+    );
+  }
+}
